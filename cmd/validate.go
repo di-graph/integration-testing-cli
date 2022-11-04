a0eb29c6251e7aabe2bcb40d7798ed83fcc5c9a6
Bahar Shah <bahar@getdigraph.com>
2022-11-04 15:13:56 -0400

    wire api call

--- cmd/validate.go
+++ cmd/validate.go
@@ -5,12 +5,16 @@ Copyright © 2022 NAME HERE <EMAIL ADDRESS>
 package cmd
 
 import (
+	"bytes"
 	"encoding/json"
+	"errors"
 	"fmt"
 	"io/ioutil"
+	"net/http"
 	"os"
 	"os/exec"
 	"strings"
+	"time"
 
 	"github.com/spf13/cobra"
 )
@@ -37,10 +41,22 @@ type ParsedTerraformPlan struct {
 	ResourceChanges []ResourceChange `json:"resource_changes"`
 }
 
-func parseTerraformPlanJSON(jsonFilePath string) error {
+type TerraformConfigValidatorInput struct {
+	TerraformPlan             ParsedTerraformPlan `json:"terraform_plan"`
+	Organization              string              `json:"organization"`
+	Repository                string              `json:"repository"`
+	TriggeringActionEventName string              `json:"event_name"`
+	IssueNumber               int                 `json:"issue_number"`
+	CommitSHA                 string              `json:"commit_sha"`
+	Ref                       string              `json:"ref"`
+}
+
+const validationURL = "https://app.getdigraph.com/api/validate/terraform"
+
+func parseTerraformPlanJSON(jsonFilePath string) (ParsedTerraformPlan, error) {
 	jsonFile, err := os.Open(jsonFilePath)
 	if err != nil {
-		return fmt.Errorf("error %s", err.Error())
+		return ParsedTerraformPlan{}, fmt.Errorf("error %s", err.Error())
 	}
 
 	jsonByteValue, _ := ioutil.ReadAll(jsonFile)
@@ -62,12 +78,52 @@ func parseTerraformPlanJSON(jsonFilePath string) error {
 		ResourceChanges: actualChanges,
 	}
 
-	fmt.Printf("%v\n", parsedPlanChangesOnly)
-
 	defer jsonFile.Close()
 
 	// cleanup by removing temp file that was written
 	os.Remove(jsonFilePath)
+	return parsedPlanChangesOnly, nil
+}
+
+func invokeDigraphValidateAPI(parsedTFPlan ParsedTerraformPlan, digraphAPIKey, organization, repository, eventName, ref, commitSHA string, issueNumber int) error {
+	requestBody := TerraformConfigValidatorInput{
+		TerraformPlan:             parsedTFPlan,
+		Organization:              organization,
+		Repository:                repository,
+		Ref:                       ref,
+		TriggeringActionEventName: eventName,
+	}
+
+	if issueNumber > 0 {
+		requestBody.IssueNumber = issueNumber
+	} else if len(commitSHA) > 0 {
+		requestBody.CommitSHA = commitSHA
+	} else {
+		return errors.New("invalid input- must specify pull request or commit sha")
+	}
+
+	requestBytes, err := json.Marshal(requestBody)
+	if err != nil {
+		return err
+	}
+
+	req, err := http.NewRequest(http.MethodPost, validationURL, bytes.NewReader(requestBytes))
+	if err != nil {
+		return err
+	}
+
+	req.Header.Set("X-Digraph-Secret-Key", digraphAPIKey)
+
+	client := http.Client{
+		Timeout: 30 * time.Second,
+	}
+
+	_, err = client.Do(req)
+	if err != nil {
+		fmt.Printf("client: error making http request: %s\n", err)
+		return err
+	}
+
 	return nil
 }
 
@@ -80,7 +136,15 @@ func validateTF() *cobra.Command {
 		RunE: func(cmd *cobra.Command, args []string) error {
 			tfPlanOutput, _ := cmd.Flags().GetString("stdOutPlan")
 			terraformAPIKey, _ := cmd.Flags().GetString("terraformAPIKey")
-			// digraphAPIKey, _ := cmd.Flags().GetString("digraphAPIKey")
+			digraphAPIKey, _ := cmd.Flags().GetString("digraphAPIKey")
+			organization, _ := cmd.Flags().GetString("organization")
+			repository, _ := cmd.Flags().GetString("repository")
+			eventName, _ := cmd.Flags().GetString("eventName")
+			ref, _ := cmd.Flags().GetString("ref")
+
+			issueNumber, _ := cmd.Flags().GetInt("issueNumber")
+			fmt.Print(issueNumber)
+			commitSHA, _ := cmd.Flags().GetString("commitSHA")
 
 			execCmd := exec.Command("/bin/sh", "./scripts/get_remote_plan_json.sh", tfPlanOutput, terraformAPIKey)
 			jsonPathBytes, err := execCmd.Output()
@@ -90,11 +154,15 @@ func validateTF() *cobra.Command {
 
 			jsonFilePath := strings.TrimSpace(strings.Replace(string(jsonPathBytes), "\r\n", "", -1))
 
-			err = parseTerraformPlanJSON(jsonFilePath)
+			parsedPlan, err := parseTerraformPlanJSON(jsonFilePath)
 			if err != nil {
 				return fmt.Errorf("error parsing JSON %s", err.Error())
 			}
 
+			err = invokeDigraphValidateAPI(parsedPlan, digraphAPIKey, organization, repository, eventName, ref, commitSHA, issueNumber)
+			if err != nil {
+				return fmt.Errorf("error calling API %s", err.Error())
+			}
 			return nil
 		},
 	}
@@ -106,7 +174,15 @@ func validateTF() *cobra.Command {
 	_ = cmd.MarkFlagRequired("terraformAPIKey")
 
 	cmd.Flags().String("digraphAPIKey", "", "Digraph API Key")
-	// _ = cmd.MarkFlagRequired("digraphAPIKey")
+	_ = cmd.MarkFlagRequired("digraphAPIKey")
+
+	cmd.Flags().String("organization", "", "Github organization")
+	cmd.Flags().String("repository", "", "Github repository")
+	cmd.Flags().String("eventName", "", "Pull or Push")
+	cmd.Flags().String("ref", "", "Branch ref")
+
+	cmd.Flags().Int("issueNumber", 0, "Pull Request Number")
+	cmd.Flags().String("commitSHA", "", "Commit SHA")
 
 	return cmd
 }
